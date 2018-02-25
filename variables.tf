161ff959cb802ab35b80446ae787c92ab0f63482
Parth Modi <parthmodi54@yahoo.com>
2018-02-25 23:20:47 +0530

    variables updated

--- variables.tf
+++ variables.tf
@@ -11,6 +11,7 @@ variable "db_username" {
 variable "db_password" {
 }
 
+# vpc variables
 variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
@@ -27,10 +28,28 @@ variable "allow_all" {
     default = "0.0.0.0/0"
 }
 
+# ec2 variables
 variable "aws_key_name" {
     default = "webserver"
 }
 
 variable "private_key_path" {
     default = "webserver.pem"
+}
+
+# cluster variables
+variable "ecs_cluster" {
+    default = "docker-demo-cluster"
+}
+
+variable "min_instance_size" {
+    default = 1
+}
+
+variable "max_instance_size" {
+    default = 3
+}
+
+variable "desired_capacity" {
+    default = 1
 }
